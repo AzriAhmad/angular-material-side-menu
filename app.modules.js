40e6fe3693e16c60d67deba2a9f66160db62f37d
Azri Ahmad <azriahmad94@gmail.com>
2016-02-01 02:47:19 -0600

    some modification

--- app.modules.js
+++ app.modules.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-
+  
   angular.module('common.services', []);
   angular.module('myMenuApp.controllers', ['common.directives']);
   angular.module('common.directives', ['common.services']);
